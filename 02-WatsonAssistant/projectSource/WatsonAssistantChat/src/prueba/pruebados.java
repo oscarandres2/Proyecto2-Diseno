a3f00abec07c9acd119c701d7445d0d2bd71e113
danielQ07 <56702587+danielQ07@users.noreply.github.com>
2020-07-15 18:41:59 -0600

    asd

--- 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/prueba/pruebados.java
+++ 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/prueba/pruebados.java
@@ -5,21 +5,38 @@ import com.google.cloud.language.v1.Document;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.cloud.language.v1.LanguageServiceClient;
 import com.google.cloud.language.v1.Sentiment;
+import com.ibm.watson.assistant.v1.Assistant;
+import com.ibm.watson.developer_cloud.service.security.IamOptions;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
+import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneOptions;
 
 public class pruebados {
 	public static void main(String[] args) throws Exception {
-	 // Instantiates a client
-	 try (LanguageServiceClient language = LanguageServiceClient.create()) {
-	
-	   // The text to analyze
-	   String text = "Hello, world!";
-	   Document doc = Document.newBuilder().setContent(text).setType(Type.PLAIN_TEXT).build();
-	
-	   // Detects the sentiment of the text
-	   Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
-	
-	   System.out.printf("Text: %s%n", text);
-	   System.out.printf("Sentiment: %s, %s%n", sentiment.getScore(), sentiment.getMagnitude());
-	 }
+		String apiKey = "0AC-rK7ZMahYaeGkPxmBhcCS1OWzrLLcxVyFjOLpDir2";
+		IamOptions iAmOptions = new IamOptions.Builder()
+				.apiKey(apiKey)
+				.build();
+		ToneAnalyzer service = new ToneAnalyzer("2017-09-21", iAmOptions);
+
+		String text =
+				  "I know the times are difficult! Our sales have been "
+				      + "disappointing for the past three quarters for our data analytics "
+				      + "product suite. We have a competitive data analytics product "
+				      + "suite in the industry. But we need to do our job selling it! "
+				      + "We need to acknowledge and fix our sales challenges. "
+				      + "We can’t blame the economy for our lack of execution! "
+				      + "We are missing critical sales opportunities. "
+				      + "Our product is in no way inferior to the competitor products. "
+				      + "Our clients are hungry for analytical tools to improve their "
+				      + "business outcomes. Economy has nothing to do with it.";
+
+				// Call the service and get the tone
+				ToneOptions toneOptions = new ToneOptions.Builder()
+				  .text(text)
+				  .build();
+
+				ToneAnalysis tone = service.tone(toneOptions).execute();
+				System.out.println(tone);
 	}
 }
