a060ac0b0c6e46631ebd6e0bf1f2c09bcb4bb5cc
Óscar Trejos Esquivel <56702153+oscarandres2@users.noreply.github.com>
2020-07-18 13:12:39 -0600

    doc

--- 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/CSV.java
+++ 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/CSV.java
@@ -6,44 +6,55 @@ import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
 import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
-
 import com.opencsv.CSVReader;
 
+
+/**
+ * @author Oscar y Daniel
+ * Clase CSV que contiene los atributos y 
+ * métodos necesarios.
+ *
+ */
 public class CSV extends Bitácora {
 
-	@Override
+  /**
+   * Método que permite crear la bitácora 
+   * de tipo CSV.
+   */
   public void crearBitacora() {
 	try{
-	   String nombre = "probandoo";
-	   String tipoArchivo = ".csv";
-	   nombre += tipoArchivo;
-	   PrintWriter pw= new PrintWriter(new File("C:\\Users\\Oscar\\OneDrive\\Escritorio\\prueba\\"+nombre));
-	   StringBuilder sb=new StringBuilder();
-	   sb.append("TipoOperación");
-	   sb.append(",");
-	   sb.append("FechaOperacion");
-	   sb.append(",");
-	   sb.append("HoraOperacion");
-	   sb.append("\r\n");
-	   sb.append("Codificacion");
-	   sb.append(",");
-	   sb.append(getFechaOperacion());
-	   sb.append(",");
-	   sb.append(getHoraOperacion());
-	   sb.append("\r\n");
-	   pw.write(sb.toString());
-	   pw.close();
-	   System.out.println("Finalizado");
+	  String nombre = "probandoo";
+	  String tipoArchivo = ".csv";
+	  nombre += tipoArchivo;
+	  PrintWriter pw= new PrintWriter(new File("C:\\Users\\Oscar\\OneDrive\\Escritorio\\prueba\\"+nombre));
+	  StringBuilder sb=new StringBuilder();
+	  sb.append("TipoOperación");
+	  sb.append(",");
+	  sb.append("FechaOperacion");
+	  sb.append(",");
+	  sb.append("HoraOperacion");
+	  sb.append("\r\n");
+	  sb.append("Codificacion");
+	  sb.append(",");
+	  sb.append(getFechaOperacion());
+	  sb.append(",");
+	  sb.append(getHoraOperacion());
+	  sb.append("\r\n");
+	  pw.write(sb.toString());
+	  pw.close();
+	  System.out.println("Finalizado");
 	   } catch (Exception e) {
 	      // TODO: handle exception
 	   }
 		
 	}
 
-	@Override
+  	/**
+  	 * Método que permite leer la bitácora 
+     * de tipo CSV.
+  	 */
 	public String leerBitacora() throws ParserConfigurationException, SAXException, IOException {
 		//Inicializa el lector del csv
 	      CSVReader reader = new CSVReader(new FileReader("C:\\Users\\Oscar\\OneDrive\\Escritorio\\prueba\\probandoo.csv"));
--- 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/XML.java
+++ 02-WatsonAssistant/projectSource/WatsonAssistantChat/src/modelo/XML.java
@@ -15,6 +15,13 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+
+/**
+ * @author Oscar y Daniel
+ * Clase XML que contiene los atributos y 
+ * métodos necesarios.
+ *
+ */
 public class XML extends Bitácora {
 
   /**
